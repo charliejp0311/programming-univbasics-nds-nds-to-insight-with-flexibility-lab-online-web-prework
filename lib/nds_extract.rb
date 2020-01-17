6707bb1da974ba09bac547e2512f4d8c76643c97
Charles Pelton <charliejp0311@gmail.com>
2020-01-17 01:00:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -83,7 +83,7 @@ def gross_per_studio(collection)
     sum_of_wwg = 0
     while cntr1 < collection.length do
       if collection[cntr1][:studio] == studios[stdos]
-        sum_of_wwg += collection[cntr1][:worldwide_gross]
+        pp collection[cntr1][:worldwide_gross]
       end
       
       cntrl += 1
